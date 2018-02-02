85e9ae393e7b5dcdfd182fd991df9e787d4d0846
JiaQi Xian <Jiaqi@JiaQis-MacBook-Pro.local>
2018-02-01 22:15:26 -0800

    initial

--- dist/index.js
+++ dist/index.js
@@ -107,12 +107,19 @@ var _Pick = require('./models/Pick');
 
 var _Pick2 = _interopRequireDefault(_Pick);
 
+var _apn = require('./apn');
+
+var _apn2 = _interopRequireDefault(_apn);
+
+var _apn3 = require('apn');
+
+var _apn4 = _interopRequireDefault(_apn3);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 //import updateTables from './queues/updateTables';
 
-// import apn from 'apn'
 var dirname = exports.dirname = __dirname;
 _mongoose2.default.connect(_config2.default.mongoURL, { useMongoClient: true });
 _mongoose2.default.Promise = _bluebird2.default;
@@ -131,19 +138,31 @@ app.use('/images/teamlogos', _express2.default.static(_path2.default.join(__dirn
 app.use('/images/teamlogos', _express2.default.static(_path2.default.join(__dirname, '/public/images/teamlogos/hockey')));
 app.use('/images/teamlogos', _express2.default.static(_path2.default.join(__dirname, '/public/images/teamlogos/sport')));
 app.use(_express2.default.static(_path2.default.resolve(__dirname, '../client/build')));
+
 app.get('/hi', function () {
 	var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(req, res) {
+		var agentNotify;
 		return regeneratorRuntime.wrap(function _callee$(_context) {
 			while (1) {
 				switch (_context.prev = _context.next) {
 					case 0:
-						_context.next = 2;
-						return (0, _updateEvents2.default)();
-
-					case 2:
-						res.json('done');
+						agentNotify = new _apn4.default.Notification({
+							sound: 'ping.aiff',
+							alert: 'test',
+							topic: 'org.reactjs.native.example.SportsAgentApp',
+							payload: { BetOrder: '1234' }
+						});
+						_context.next = 3;
+						return _apn2.default.send(agentNotify, '23f8b6c64012cf886b5d3b2e588c40d1e8e476439ea6c94a781d479d764464bf').then(function (res) {
+							return console.log(res);
+						});
 
 					case 3:
+						_apn2.default.shutdown();
+
+						res.json('done');
+
+					case 5:
 					case 'end':
 						return _context.stop();
 				}
@@ -155,6 +174,7 @@ app.get('/hi', function () {
 		return _ref.apply(this, arguments);
 	};
 }());
+
 var userCounter = [];
 _nodeSchedule2.default.scheduleJob('*/3 * * * *', _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee2() {
 	return regeneratorRuntime.wrap(function _callee2$(_context2) {
