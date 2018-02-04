d3247dc767de5089dcef33ce315d630993e1e180
JiaQi Xian <Jiaqi@JiaQis-MacBook-Pro.local>
2018-02-03 17:01:44 -0800

    initial

--- dist/index.js
+++ dist/index.js
@@ -259,8 +259,27 @@ ws.listen(process.env.PORT, function () {
 				userCounter.push(webSocket.upgradeReq.headers['sec-websocket-key']);
 			}
 			// // eslint-disable-next-line
-			// console.log(userCounter.length);
-			// if(userCounter.length === 1){ (async() => await updateEvents())();}
+			console.log(userCounter.length);
+			if (userCounter.length === 1) {
+				_asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee4() {
+					return regeneratorRuntime.wrap(function _callee4$(_context4) {
+						while (1) {
+							switch (_context4.prev = _context4.next) {
+								case 0:
+									_context4.next = 2;
+									return (0, _updateEvents2.default)();
+
+								case 2:
+									return _context4.abrupt('return', _context4.sent);
+
+								case 3:
+								case 'end':
+									return _context4.stop();
+							}
+						}
+					}, _callee4, undefined);
+				}))();
+			}
 		},
 		onDisconnect: function onDisconnect(webSocket) {
 			userCounter = userCounter.filter(function (user) {
