4c4d73f350a5a1508041a17db8dc64725b3ca97f
JiaQi Xian <Jiaqi@JiaQis-MacBook-Pro.local>
2018-02-08 19:28:10 -0800

    initial

--- dist/services/updateEvents/jsonOdd.js
+++ dist/services/updateEvents/jsonOdd.js
@@ -54,7 +54,7 @@ var axiosJsonOdd = _axios2.default.create({ headers: { 'JsonOdds-API-Key': _conf
 
 var updateEventOddFromJsonOdd = function () {
 	var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
-		var jsonOddSports, jsonEvents, _iteratorNormalCompletion, _didIteratorError, _iteratorError, _iterator, _step, event, jsonSport, theSport, theMatchTime, theLeague, theRegion, theAwayPitcher, theHomePitcher, theAwayROT, theHomeROT, awayLogo, homeLogo, folderName, awayLogoExists, homeLogoExists, _iteratorNormalCompletion2, _didIteratorError2, _iteratorError2, _iterator2, _step2, odd, thePeriod, theUniqueID, newEvent;
+		var jsonOddSports, jsoonEvents, nba, allSports, _iteratorNormalCompletion, _didIteratorError, _iteratorError, _iterator, _step, event, jsonSport, theSport, theMatchTime, theLeague, theRegion, theAwayPitcher, theHomePitcher, theAwayROT, theHomeROT, awayLogo, homeLogo, folderName, awayLogoExists, homeLogoExists, _iteratorNormalCompletion2, _didIteratorError2, _iteratorError2, _iterator2, _step2, odd, thePeriod, theUniqueID, newEvent;
 
 		return regeneratorRuntime.wrap(function _callee$(_context) {
 			while (1) {
@@ -82,33 +82,48 @@ var updateEventOddFromJsonOdd = function () {
 
 					case 8:
 						jsonOddSports = _context.sent;
-						_context.next = 11;
-						return axiosJsonOdd.get('https://jsonodds.com/api/odds?source=4').then(function (res) {
+						jsoonEvents = [];
+						_context.next = 12;
+						return axiosJsonOdd.get('https://jsonodds.com/api/odds/nba?source=4').then(function (res) {
 							return res.data;
 						}).catch(function () {
 							return [];
 						});
 
-					case 11:
-						jsonEvents = _context.sent;
+					case 12:
+						nba = _context.sent;
+						_context.next = 15;
+						return axiosJsonOdd.get('https://jsonodds.com/api/odds?source=3').then(function (res) {
+							return res.data.filter(function (event) {
+								return event.Sport !== 1;
+							});
+						}).catch(function () {
+							return [];
+						});
+
+					case 15:
+						allSports = _context.sent;
+
+
+						jsoonEvents = nba.concat(allSports);
 
 						if (!_lodash2.default.isEmpty(jsonEvents)) {
-							_context.next = 14;
+							_context.next = 19;
 							break;
 						}
 
 						return _context.abrupt('return');
 
-					case 14:
+					case 19:
 						_iteratorNormalCompletion = true;
 						_didIteratorError = false;
 						_iteratorError = undefined;
-						_context.prev = 17;
+						_context.prev = 22;
 						_iterator = jsonEvents[Symbol.iterator]();
 
-					case 19:
+					case 24:
 						if (_iteratorNormalCompletion = (_step = _iterator.next()).done) {
-							_context.next = 89;
+							_context.next = 94;
 							break;
 						}
 
@@ -116,50 +131,50 @@ var updateEventOddFromJsonOdd = function () {
 						jsonSport = _lodash2.default.find(jsonOddSports, { idx: event.Sport, activate: true });
 
 						if (jsonSport) {
-							_context.next = 24;
+							_context.next = 29;
 							break;
 						}
 
-						return _context.abrupt('continue', 86);
+						return _context.abrupt('continue', 91);
 
-					case 24:
+					case 29:
 						theSport = jsonSport.sport;
 						theMatchTime = _moment2.default.utc(event.MatchTime);
 
 						if (!(0, _moment2.default)().add(5, 'days').endOf('day').isBefore(theMatchTime)) {
-							_context.next = 28;
+							_context.next = 33;
 							break;
 						}
 
-						return _context.abrupt('continue', 86);
+						return _context.abrupt('continue', 91);
 
-					case 28:
+					case 33:
 						if (!(0, _moment2.default)().isAfter(theMatchTime)) {
-							_context.next = 30;
-							break;
-						}
-
-						return _context.abrupt('continue', 86);
-
-					case 30:
-						if (!(0, _moment2.default)().add(5, 'd').endOf('day').isBefore(theMatchTime)) {
-							_context.next = 32;
-							break;
-						}
-
-						return _context.abrupt('continue', 86);
-
-					case 32:
-						theLeague = event.Sport === 7 ? event.hasOwnProperty('League') && event.League.hasOwnProperty('Name') ? event.League.Name : '' : jsonSport.league;
-
-						if (!(theLeague === '')) {
 							_context.next = 35;
 							break;
 						}
 
-						return _context.abrupt('continue', 86);
+						return _context.abrupt('continue', 91);
 
 					case 35:
+						if (!(0, _moment2.default)().add(5, 'd').endOf('day').isBefore(theMatchTime)) {
+							_context.next = 37;
+							break;
+						}
+
+						return _context.abrupt('continue', 91);
+
+					case 37:
+						theLeague = event.Sport === 7 ? event.hasOwnProperty('League') && event.League.hasOwnProperty('Name') ? event.League.Name : '' : jsonSport.league;
+
+						if (!(theLeague === '')) {
+							_context.next = 40;
+							break;
+						}
+
+						return _context.abrupt('continue', 91);
+
+					case 40:
 						theRegion = event.Sport === 7 ? event.DisplayRegion : null;
 						theAwayPitcher = theSport === 'Baseball' ? !_lodash2.default.isEmpty(event.AwayPitcher) ? event.AwayPitcher : 'Action' : null;
 						theHomePitcher = theSport === 'Baseball' ? !_lodash2.default.isEmpty(event.HomePitcher) ? event.HomePitcher : 'Action' : null;
@@ -185,48 +200,48 @@ var updateEventOddFromJsonOdd = function () {
 							detail: event.AwayTeam + ' ' + theAwayROT + ' vs ' + event.HomeTeam + ' ' + theHomeROT
 						};
 						folderName = theLeague === 'NCAAB' || theLeague === 'NCAAF' ? 'NCAA' : theSport;
-						_context.next = 45;
+						_context.next = 50;
 						return (0, _fileExists2.default)(_index.dirname + ('/public/images/teamlogos/' + folderName + '/' + awayLogo.name + '.png'));
 
-					case 45:
+					case 50:
 						awayLogoExists = _context.sent;
 
 						if (awayLogoExists) {
-							_context.next = 49;
+							_context.next = 54;
 							break;
 						}
 
-						_context.next = 49;
+						_context.next = 54;
 						return _LogoCollect2.default.findOneAndUpdate({ name: awayLogo.name }, awayLogo, { upsert: true });
 
-					case 49:
-						_context.next = 51;
+					case 54:
+						_context.next = 56;
 						return (0, _fileExists2.default)(_index.dirname + ('/public/images/teamlogos/' + folderName + '/' + homeLogo.name + '.png'));
 
-					case 51:
+					case 56:
 						homeLogoExists = _context.sent;
 
 						if (homeLogoExists) {
-							_context.next = 55;
+							_context.next = 60;
 							break;
 						}
 
-						_context.next = 55;
+						_context.next = 60;
 						return _LogoCollect2.default.findOneAndUpdate({ name: homeLogo.name }, homeLogo, { upsert: true });
 
-					case 55:
+					case 60:
 						//
 
 
 						_iteratorNormalCompletion2 = true;
 						_didIteratorError2 = false;
 						_iteratorError2 = undefined;
-						_context.prev = 58;
+						_context.prev = 63;
 						_iterator2 = event.Odds[Symbol.iterator]();
 
-					case 60:
+					case 65:
 						if (_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done) {
-							_context.next = 72;
+							_context.next = 77;
 							break;
 						}
 
@@ -234,13 +249,13 @@ var updateEventOddFromJsonOdd = function () {
 						thePeriod = (0, _renameEventPeriod2.default)(odd.OddType);
 
 						if (!(thePeriod === 'unknow')) {
-							_context.next = 65;
+							_context.next = 70;
 							break;
 						}
 
-						return _context.abrupt('continue', 69);
+						return _context.abrupt('continue', 74);
 
-					case 65:
+					case 70:
 						theUniqueID = (0, _moment2.default)(theMatchTime).format('MMDDYY') + '_' + theAwayROT + '_' + theHomeROT + '_' + theSport + '_' + thePeriod.replace(/\s/g, '');
 						newEvent = {
 							ID: odd.ID,
@@ -282,103 +297,103 @@ var updateEventOddFromJsonOdd = function () {
 							status: 'Pending',
 							updatedAt: (0, _moment2.default)()
 						};
-						_context.next = 69;
+						_context.next = 74;
 						return _Event2.default.findOneAndUpdate({ uniqueID: newEvent.uniqueID }, newEvent, { upsert: true });
 
-					case 69:
-						_iteratorNormalCompletion2 = true;
-						_context.next = 60;
-						break;
-
-					case 72:
-						_context.next = 78;
-						break;
-
 					case 74:
-						_context.prev = 74;
-						_context.t0 = _context['catch'](58);
+						_iteratorNormalCompletion2 = true;
+						_context.next = 65;
+						break;
+
+					case 77:
+						_context.next = 83;
+						break;
+
+					case 79:
+						_context.prev = 79;
+						_context.t0 = _context['catch'](63);
 						_didIteratorError2 = true;
 						_iteratorError2 = _context.t0;
 
-					case 78:
-						_context.prev = 78;
-						_context.prev = 79;
+					case 83:
+						_context.prev = 83;
+						_context.prev = 84;
 
 						if (!_iteratorNormalCompletion2 && _iterator2.return) {
 							_iterator2.return();
 						}
 
-					case 81:
-						_context.prev = 81;
+					case 86:
+						_context.prev = 86;
 
 						if (!_didIteratorError2) {
-							_context.next = 84;
+							_context.next = 89;
 							break;
 						}
 
 						throw _iteratorError2;
 
-					case 84:
-						return _context.finish(81);
-
-					case 85:
-						return _context.finish(78);
-
-					case 86:
-						_iteratorNormalCompletion = true;
-						_context.next = 19;
-						break;
-
 					case 89:
-						_context.next = 95;
-						break;
+						return _context.finish(86);
+
+					case 90:
+						return _context.finish(83);
 
 					case 91:
-						_context.prev = 91;
-						_context.t1 = _context['catch'](17);
+						_iteratorNormalCompletion = true;
+						_context.next = 24;
+						break;
+
+					case 94:
+						_context.next = 100;
+						break;
+
+					case 96:
+						_context.prev = 96;
+						_context.t1 = _context['catch'](22);
 						_didIteratorError = true;
 						_iteratorError = _context.t1;
 
-					case 95:
-						_context.prev = 95;
-						_context.prev = 96;
+					case 100:
+						_context.prev = 100;
+						_context.prev = 101;
 
 						if (!_iteratorNormalCompletion && _iterator.return) {
 							_iterator.return();
 						}
 
-					case 98:
-						_context.prev = 98;
+					case 103:
+						_context.prev = 103;
 
 						if (!_didIteratorError) {
-							_context.next = 101;
+							_context.next = 106;
 							break;
 						}
 
 						throw _iteratorError;
 
-					case 101:
-						return _context.finish(98);
+					case 106:
+						return _context.finish(103);
 
-					case 102:
-						return _context.finish(95);
+					case 107:
+						return _context.finish(100);
 
-					case 103:
-						_context.next = 109;
+					case 108:
+						_context.next = 114;
 						break;
 
-					case 105:
-						_context.prev = 105;
+					case 110:
+						_context.prev = 110;
 						_context.t2 = _context['catch'](0);
-						_context.next = 109;
+						_context.next = 114;
 						return _SystemLog2.default.create({ title: 'update json odd event failed', content: _context.t2, status: 'danger' });
 
-					case 109:
+					case 114:
 					case 'end':
 						return _context.stop();
 				}
 			}
-		}, _callee, undefined, [[0, 105], [17, 91, 95, 103], [58, 74, 78, 86], [79,, 81, 85], [96,, 98, 102]]);
+		}, _callee, undefined, [[0, 110], [22, 96, 100, 108], [63, 79, 83, 91], [84,, 86, 90], [101,, 103, 107]]);
 	}));
 
 	return function updateEventOddFromJsonOdd() {
