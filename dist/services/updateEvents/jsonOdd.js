cb7e1147726fe0493032a621ae703fd9ce8f84c8
JiaQi Xian <Jiaqi@JiaQis-MacBook-Pro.local>
2018-02-25 17:06:16 -0800

    initial

--- dist/services/updateEvents/jsonOdd.js
+++ dist/services/updateEvents/jsonOdd.js
@@ -52,7 +52,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
-var axiosJsonOdd = _axios2.default.create({ headers: { 'JsonOdds-API-Key': _config2.default.jsonOddApiKey } });
+var axiosJsonOdd = _axios2.default.create({ headers: { 'x-api-key': _config2.default.jsonOddApiKey } });
 
 var updateEventOddFromJsonOdd = function () {
 	var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee() {
